d088f150fba00462a1e6ef8751d077edcfc1952f
Nathan Mande <neodarksoulink@gmail.com>
2024-04-23 22:50:32 +0200

    refactor: add methods definitions

--- src/lib/svelte-machine/physic.ts
+++ src/lib/svelte-machine/physic.ts
@@ -13,7 +13,6 @@ import {
 import type RAPIER from '@dimforge/rapier3d';
 
 import { events } from '$lib/experience/static';
-import { SvelteMachineExperience } from '.';
 
 export interface Object3DWithGeometry extends Object3D {
 	geometry?: BufferGeometry;
@@ -25,19 +24,62 @@ export interface Object3DWithGeometry extends Object3D {
  * @docs https://rapier.rs/docs/api/javascript/JavaScript3D/
  */
 export class Physic extends EventTarget {
-	private _experience = new SvelteMachineExperience();
-	private _app = this._experience.app;
-
 	private _vector = new Vector3();
-	private _vectorZero = new Vector3();
 	private _quaternion = new Quaternion();
 	private _matrix = new Matrix4();
 	private _scale = new Vector3(1, 1, 1);
 
+	/**
+	 * @description `Rapier3D.js`.
+	 *
+	 * @type {typeof RAPIER}
+	 */
 	public rapier!: typeof RAPIER;
+	/**
+	 * @description {@link RAPIER.World} instance.
+	 *
+	 * @type {RAPIER.World}
+	 */
 	public world!: RAPIER.World;
-	public objects: Object3DWithGeometry[] = [];
-	public objectMap = new WeakMap<WeakKey, RAPIER.RigidBody | RAPIER.RigidBody[]>();
+	/**
+	 * @description List of {@link Object3D} with physic applied.
+	 *
+	 * @type {typeof RAPIER}
+	 */
+	public dynamicObjects: Object3DWithGeometry[] = [];
+	/**
+	 * @description {@link WeakMap} of dynamic objects {@link RAPIER.RigidBody}
+	 *
+	 * @type {WeakMap<WeakKey, RAPIER.RigidBody | RAPIER.RigidBody[]>}
+	 */
+	public dynamicObjectMap = new WeakMap<WeakKey, RAPIER.RigidBody | RAPIER.RigidBody[]>();
+
+	/**
+	 * @description Add the specified `object` to the physic `dynamicObjects` map.
+	 *
+	 * @param object `Object3D` based.
+	 * @param mass Physic object mass.
+	 * @param restitution Physic Object restitution.
+	 */
+	private _addObject(object: Object3DWithGeometry, mass = 0, restitution = 0) {
+		const { colliderDesc } = this.getShape(object);
+		if (!colliderDesc) return;
+
+		colliderDesc.setMass(mass);
+		colliderDesc.setRestitution(restitution);
+
+		const body =
+			object instanceof InstancedMesh
+				? this.createInstancedBody(object, colliderDesc, mass)
+				: this.createBody(colliderDesc, object.position, object.quaternion, mass);
+
+		if (mass > 0) {
+			this.dynamicObjects.push(object);
+			this.dynamicObjectMap.set(object, body);
+		}
+
+		return body;
+	}
 
 	public async construct() {
 		this.rapier = (await import('@dimforge/rapier3d')).default;
@@ -48,6 +90,11 @@ export class Physic extends EventTarget {
 		this.dispatchEvent(new Event(events.CONSTRUCTED));
 	}
 
+	/**
+	 * Apply physic the specified object.
+	 *
+	 * @deprecated
+	 */
 	public applyPhysic(item: Object3DWithGeometry) {
 		if (!this.rapier || !this.world) return;
 
@@ -63,27 +110,44 @@ export class Physic extends EventTarget {
 		return { ...colliderDescData, collider, rigidBody, rigidBodyDesc };
 	}
 
-	public addScene(scene: Object3DWithGeometry) {
-		scene.traverse((child) => {
+	/**
+	 * Add objects children from the specified {@link Object3D} to the physic world using the userData.
+	 *
+	 * @param {Object3D} object Object3D based.
+	 *
+	 * @example ```ts
+	 *  const floor = new Mesh(
+	 *    new BoxGeometry(500, 5, 500),
+	 *    new MeshBasicMaterial({})
+	 *  );
+	 *  floor.position.setY(-10);
+	 *  floor.userData.physics = { mass: 0, restitution: restitution };
+	 *
+	 *  rapierPhysicsHelper?.addToWorld(floor, 0);
+	 * ```
+	 */
+	public addSceneToWorld(object: Object3DWithGeometry) {
+		object.traverse((child) => {
 			if (!(child instanceof Object3D) || !child.userData.physics) return;
 
 			const physics = child.userData.physics;
 
 			if (!physics) return;
 
-			this.addObject(child, physics.mass, physics.restitution);
+			this._addObject(child, physics.mass, physics.restitution);
 		});
 	}
 
 	/**
-	 * @description Apply physic to the specify object. Add the object to the physic `world`.
+	 * @description Apply physic to the specified object. Add the object to the physic `world`.
 	 *
 	 * @param {Object3D} object Object3D based.
 	 * @param {number} mass Physic mass.
 	 * @param {number} restitution Physic restitution.
 	 */
 	public addToWorld(object: Object3D, mass = 0, restitution = 0) {
-		if (object instanceof Object3D) this.addObject(object, Number(mass), Number(restitution));
+		if (object instanceof Object3D)
+			return this._addObject(object, Number(mass), Number(restitution));
 	}
 
 	/**
@@ -163,24 +227,13 @@ export class Physic extends EventTarget {
 		};
 	}
 
-	public addObject(mesh: Object3DWithGeometry, mass = 0, restitution = 0) {
-		const { colliderDesc } = this.getShape(mesh);
-		if (!colliderDesc) return;
-
-		colliderDesc.setMass(mass);
-		colliderDesc.setRestitution(restitution);
-
-		const body =
-			mesh instanceof InstancedMesh
-				? this.createInstancedBody(mesh, colliderDesc, mass)
-				: this.createBody(colliderDesc, mesh.position, mesh.quaternion, mass);
-
-		if (mass <= 0) return;
-
-		this.objects.push(mesh);
-		this.objectMap.set(mesh, body);
-	}
-
+	/**
+	 * @description Create {@link RAPIER.RigidBody} for each instance of the {@link InstancedMesh} specified mesh
+	 *
+	 * @param {InstancedMesh} mesh {@link InstancedMesh}
+	 * @param {RAPIER.ColliderDesc} colliderDesc {@link RAPIER.ColliderDesc}
+	 * @param {number | undefined} mass
+	 */
 	public createInstancedBody(mesh: InstancedMesh, colliderDesc: RAPIER.ColliderDesc, mass: number) {
 		const array = mesh.instanceMatrix.array;
 		const bodies = [];
@@ -193,15 +246,23 @@ export class Physic extends EventTarget {
 		return bodies;
 	}
 
-	createBody(
+	/**
+	 * @description Create {@link RAPIER.RigidBody} for the specified {@link RAPIER.Collider}
+	 *
+	 * @param {RAPIER.ColliderDesc} colliderDesc {@link RAPIER.ColliderDesc}
+	 * @param {RAPIER.Vector3} position {@link RAPIER.Vector3}
+	 * @param {RAPIER.Rotation} rotation {@link RAPIER.Rotation}
+	 * @param {number | undefined} mass
+	 */
+	public createBody(
 		colliderDesc: RAPIER.ColliderDesc,
 		position: RAPIER.Vector3,
-		quaternion?: RAPIER.Rotation | null,
+		rotation?: RAPIER.Rotation | null,
 		mass = 0
 	) {
 		const desc = mass > 0 ? this.rapier.RigidBodyDesc.dynamic() : this.rapier.RigidBodyDesc.fixed();
 		desc.setTranslation(position.x, position.y, position.z);
-		if (quaternion) desc.setRotation(quaternion);
+		if (rotation) desc.setRotation(rotation);
 
 		const body = this.world.createRigidBody(desc);
 		this.world.createCollider(colliderDesc, body);
@@ -209,48 +270,70 @@ export class Physic extends EventTarget {
 		return body;
 	}
 
-	getBodyFromMesh(mesh: Mesh | InstancedMesh, index = 0) {
-		const _body = this.objectMap.get(mesh);
+	/**
+	 *
+	 * @param object
+	 * @param index
+	 * @returns
+	 */
+	getBodyFromObject(object: Object3DWithGeometry, index = 0) {
+		const _body = this.dynamicObjectMap.get(object);
 		let body: RAPIER.RigidBody;
 
 		if (!_body) return undefined;
-		if (mesh instanceof InstancedMesh && typeof _body === 'object')
+		if (object instanceof InstancedMesh && typeof _body === 'object')
 			body = (_body as RAPIER.RigidBody[])[index];
 		else body = _body as RAPIER.RigidBody;
 
 		return body;
 	}
 
-	setMeshPosition(mesh: Mesh | InstancedMesh, position: RAPIER.Vector3, index = 0) {
-		const body = this.getBodyFromMesh(mesh, index);
+	/**
+	 *
+	 * @param object
+	 * @param position
+	 * @param index
+	 * @returns
+	 */
+	setObjectPosition(object: Object3DWithGeometry, position: RAPIER.Vector3, index = 0) {
+		const body = this.getBodyFromObject(object, index);
 		if (!body) return;
 
-		const _vectorZero = new this.rapier.Vector3(
-			this._vectorZero.x,
-			this._vectorZero.y,
-			this._vectorZero.z
-		);
+		const _vectorZero = new this.rapier.Vector3(0, 0, 0);
 		body.setAngvel(_vectorZero, true);
 		body.setLinvel(_vectorZero, true);
 		body.setTranslation(position, true);
+
+		return body;
 	}
 
-	setMeshVelocity(mesh: Mesh | InstancedMesh, velocity: RAPIER.Vector3, index = 0) {
-		const body = this.getBodyFromMesh(mesh, index);
+	/**
+	 *
+	 * @param object
+	 * @param velocity
+	 * @param index
+	 */
+	setObjectVelocity(object: Object3DWithGeometry, velocity: RAPIER.Vector3, index = 0) {
+		const body = this.getBodyFromObject(object, index);
 		if (!body) return;
 
 		body.setLinvel(velocity, true);
+
+		return body;
 	}
 
+	/**
+	 * @description Update the dynamic objects physics.
+	 */
 	public update() {
 		this.world.step();
 
-		for (let i = 0, l = this.objects.length; i < l; i++) {
-			const mesh = this.objects[i];
+		for (let i = 0, l = this.dynamicObjects.length; i < l; i++) {
+			const mesh = this.dynamicObjects[i];
 
 			if (mesh instanceof InstancedMesh) {
 				const array = mesh.instanceMatrix.array;
-				const bodies = this.objectMap.get(mesh) as RAPIER.RigidBody[];
+				const bodies = this.dynamicObjectMap.get(mesh) as RAPIER.RigidBody[];
 
 				for (let j = 0; j < bodies.length; j++) {
 					const body = bodies[j];
@@ -264,7 +347,7 @@ export class Physic extends EventTarget {
 				mesh.instanceMatrix.needsUpdate = true;
 				mesh.computeBoundingSphere();
 			} else {
-				const body = this.objectMap.get(mesh) as RAPIER.RigidBody;
+				const body = this.dynamicObjectMap.get(mesh) as RAPIER.RigidBody;
 
 				mesh.position.copy(body.translation());
 				mesh.quaternion.copy(body.rotation());
